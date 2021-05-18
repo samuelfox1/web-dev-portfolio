bd2b9fe4ca88c3253663ed2f63726537b5211ea0
Samuel Fox <samueljasonfox@gmail.com>
2021-05-18 09:57:10 -0700

    formatting for index & app.js

--- src/components/Shield/index.js
+++ src/components/Shield/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Image } from '../Elements/Elements'
 
 export default function Shield({ data }) {
     return (
 
-        <img
+        <Image
             className="z-depth-3 shield"
             src={data.src}
             alt={data.alt} />
